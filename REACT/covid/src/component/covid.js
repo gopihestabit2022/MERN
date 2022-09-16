fd2eea62efe17c4f8a060ed38eb854985f0500e3
gopi27nov <gopikumar27nov@gmail.com>
2022-09-17 00:05:41 +0530

    commit by gopi
    commit by gopi

--- REACT/covid/src/component/covid.js
+++ REACT/covid/src/component/covid.js
@@ -21,29 +21,41 @@ const Covid = () => {
             <div className="container">
                 <div className="row" >
                     <h1>Covid Cases Active !!!</h1>
-                    <div className="col-md-3">
-                        <h4>My Country</h4>
-                        <p>India</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>My Country</h4>
+                            <p>India</p>
+                         </div>   
                     </div>
-                    <div className="col-md-3">
-                        <h4>Active Case</h4>
-                        <p>{data.active}</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>Active Case</h4>
+                            <p>{data.active}</p>
+                        </div>  
                     </div>
-                    <div className="col-md-3">
-                        <h4>Confirmed Case</h4>
-                        <p>{data.confirmed}</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>Confirmed Case</h4>
+                            <p>{data.confirmed}</p>
+                        </div>  
                     </div>
-                    <div className="col-md-3">
-                        <h4>Total Death</h4>
-                        <p>{data.recovered}</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>Total Death</h4>
+                            <p>{data.recovered}</p>
+                        </div>  
                     </div>
-                    <div className="col-md-3">
-                        <h4>Total Death</h4>
-                        <p>{data.deaths}</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>Total Death</h4>
+                            <p>{data.deaths}</p>
+                        </div>  
                     </div>
-                    <div className="col-md-3">
-                        <h4>Last Updated</h4>
-                        <p>{data.lastupdatedtime}</p>
+                    <div className="col-md-4">
+                        <div className="inner-card">
+                            <h4>Last Updated</h4>
+                            <p>{data.lastupdatedtime}</p>
+                        </div>  
                     </div>
 
                 </div>
