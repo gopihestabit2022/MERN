d873dab008c6b935096020cc3e4cf3aafde37995
gopihestabit2022 <gopi.hestabit@gmail.com>
2022-09-16 19:56:28 +0530

    COVID tracker

--- REACT/covid/src/component/covid.js
+++ REACT/covid/src/component/covid.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Covid = () => {
 
+    const [data, setData] = useState([]);
+
     const covidData = async() => {
         const res = await fetch('https://api.covid19india.org/data.json', {mode:'cors'});
-        console.log(res);
+        const data = await res.json();
+        console.log(data.statewise[0]);
+        setData(data.statewise[0]);
     }
     
     useEffect(() => {
@@ -16,15 +20,32 @@ const Covid = () => {
         <div className="my-card">
             <div className="container">
                 <div className="row" >
-                    <div className="col-md-4">
-                        <h4>Hello</h4>
+                    <h1>Covid Cases Active !!!</h1>
+                    <div className="col-md-3">
+                        <h4>My Country</h4>
+                        <p>India</p>
                     </div>
-                    <div className="col-md-4">
-                        <h4>Hello</h4>
+                    <div className="col-md-3">
+                        <h4>Active Case</h4>
+                        <p>{data.active}</p>
                     </div>
-                    <div className="col-md-4">
-                        <h4>Hello</h4>
+                    <div className="col-md-3">
+                        <h4>Confirmed Case</h4>
+                        <p>{data.confirmed}</p>
                     </div>
+                    <div className="col-md-3">
+                        <h4>Total Death</h4>
+                        <p>{data.recovered}</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h4>Total Death</h4>
+                        <p>{data.deaths}</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h4>Last Updated</h4>
+                        <p>{data.lastupdatedtime}</p>
+                    </div>
+
                 </div>
             </div>
         </div>
